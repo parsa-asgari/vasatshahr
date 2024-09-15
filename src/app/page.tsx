e4f70a893e15df4f14db69d6c8d3ebbbf1570abd
Parsa asgari <parsa.asgari3264@gmail.com>
2024-09-15 11:28:10 +0000

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import Image from "next/image";
-
-import { HeroSection as HeroSectionAbout } from "@/components/hero-section-about-us";
-import { HeroSection as HeroSectionWorkshops } from "@/components/hero-section-workshops";
-import { HeroSection as HeroSectionSession } from "@/components/hero-section-sessions";
-import { HeroSection as HeroSectionEvent } from "@/components/hero-section-events";
-import { HeroSection as HeroSectionPodcast } from "@/components/hero-section-podcast";
-
-import Content from "@/components/content";
-import Feature from "@/components/feature";
-import Contact from "@/components/contact";
-
 import PageContainer from '@/components/page-container'
 
 export default function Home() {
--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -5,7 +5,14 @@ import { HeroSection as HeroSectionAbout } from "@/components/hero-section-about
 import { HeroSection as HeroSectionWorkshops } from "@/components/hero-section-workshops";
 import { HeroSection as HeroSectionSession } from "@/components/hero-section-sessions";
 import { HeroSection as HeroSectionEvent } from "@/components/hero-section-events";
+import { HeroSection as HeroSectionMagazine } from "@/components/hero-section-magazine";
 import { HeroSection as HeroSectionPodcast } from "@/components/hero-section-podcast";
+
+import Image from "next/image";
+
+import Content from "@/components/content";
+import Feature from "@/components/feature";
+import Contact from "@/components/contact";
 
 export default function PageContainer() {
 
@@ -13,8 +20,9 @@ export default function PageContainer() {
         <main>
             <HeroSectionAbout />
             <HeroSectionWorkshops />
-            <HeroSectionEvent  />
+            <HeroSectionEvent />
             <HeroSectionSession />
+            <HeroSectionMagazine />
             <HeroSectionPodcast />
         </main>
     );
